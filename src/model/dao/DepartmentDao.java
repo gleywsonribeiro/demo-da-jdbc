cac0ca2548c196aedfd315abfb5598bbe76ecf85
Gleywson Ribeiro <gleywson@gmail.com>
2020-01-03 20:27:24 -0300

    DepartmentDao and SellerDao interfaces

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.dao;
+
+import java.util.List;
+import model.entities.Department;
+
+/**
+ *
+ * @author gleyw
+ */
+public interface DepartmentDao {
+
+    void insert(Department department);
+
+    void update(Department department);
+
+    void deleteById(Integer id);
+
+    List<Department> findAll();
+
+}
---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.dao;
+
+import java.util.List;
+import model.entities.Seller;
+
+/**
+ *
+ * @author gleyw
+ */
+public interface SellerDao {
+     void insert(Seller seller);
+
+    void update(Seller seller);
+
+    void deleteById(Integer id);
+
+    List<Seller> findAll();
+}
