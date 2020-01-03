758a787345d5cbbfe5aecb49ee982043196b3411
Gleywson Ribeiro <gleywson@gmail.com>
2020-01-02 22:13:18 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,12 +5,19 @@
  */
 package application;
 
+import java.util.Date;
+import model.entities.Department;
+import model.entities.Seller;
+
 /**
  *
  * @author gleyw
  */
 public class Program {
     public static void main(String[] args) {
+        Department d = new Department(1, "Books");
+        Seller s = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, d);
         
+        System.out.println(s);
     }
 }
