64975aa2d2e7afb68e9863f6c844fe1b20a77868
Gleywson Ribeiro <gleywson@gmail.com>
2020-01-03 22:04:00 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,12 +16,12 @@ import model.entities.Seller;
  * @author gleyw
  */
 public class Program {
+
     public static void main(String[] args) {
-        
+
         SellerDao sellerDao = DaoFactory.createSellerDao();
-        
+        System.out.println("=== TEST 1: Seller findById =====");
         Seller seller = sellerDao.findById(3);
-        
         System.out.println(seller);
     }
 }
